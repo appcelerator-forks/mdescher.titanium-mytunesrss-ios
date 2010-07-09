ba604f4adb4674aa9222a2edec03d67f4b0156c4
Michael Descher <mdescher@codewave.de>
2010-07-09 19:06:16 +0000

    display only supported tracks, so nothing else can be played
    git-svn-id: svn://svn.codewave.de/titanium/MyTunesRSS/trunk@5532 6cd82e91-2702-0410-863d-86aa3370aca5

--- Resources/win_tracklist.js
+++ Resources/win_tracklist.js
@@ -17,36 +17,41 @@ if (!items) {
     items = win.ajaxResult.tracks;
 }
 for (var i = 0; i < items.length; i++) {
-    var row = Titanium.UI.createTableViewRow({hasChild:true,height:48,className:'track_row'});
-    var trackName = Titanium.UI.createLabel({text:getDisplayName(items[i].name),top:4,left:4,height:24,font:{fontSize:16,fontWeight:'bold'}});
-    var artistName = Titanium.UI.createLabel({text:getDisplayName(items[i].artist),bottom:4,left:4,height:18,font:{fontSize:12}});
-    row.add(trackName);
-    row.add(artistName);
-
-    row.jsonItem = items[i];
-    tableData.push(row);
-}
-tableView.setData(tableData);
-
-tableView.addEventListener('click', function(e) {
-    var items = win.ajaxResult.results;
-    if (!items) {
-        items = win.ajaxResult.tracks;
+    if (items[i].mediaType === 'Audio' || items[i].mediaType === 'Video') {
+        var row = Titanium.UI.createTableViewRow({hasChild:true,height:48,className:'track_row'});
+        var trackName = Titanium.UI.createLabel({text:getDisplayName(items[i].name),top:4,left:4,height:24,font:{fontSize:16,fontWeight:'bold'}});
+        var artistName = Titanium.UI.createLabel({text:getDisplayName(items[i].artist),bottom:4,left:4,height:18,font:{fontSize:12}});
+        row.add(trackName);
+        row.add(artistName);
+        row.jsonItem = items[i];
+        tableData.push(row);
     }
-    ajaxCall('LoginService.ping', null, function(result, error) {
-        if (!result && !error) {
-            Titanium.UI.createAlertDialog({message:'No response from server, please make sure the server is running.',buttonNames:['Ok']}).show();
-        } else {
-            if (items[e.index].mediaType === 'Video') {
-                Titanium.App.fireEvent('mytunesrss_stop');
-                Titanium.UI.createWindow({url:'win_videoplayer.js',data:items[e.index].playbackUrl,backgroundColor:'#000'}).open();
-            } else if (items[e.index].mediaType === 'Audio') {
-                Titanium.App.fireEvent('mytunesrss_playlist', {playlist:items,index:e.index});
-            } else {
-                Titanium.UI.createAlertDialog({message:'The media type "' + currentPlaylist[currentPlaylistIndex].mediaType + '" is not supported.',buttonNames:['Ok']}).show();
-            }
+}
+if (tableData.length == 0) {
+    tableView.touchEnabled = false;
+    tableData.push(Titanium.UI.createTableViewRow({height:48,className:'track_row',title:'No supported tracks'}));
+} else {
+    tableView.addEventListener('click', function(e) {
+        var items = win.ajaxResult.results;
+        if (!items) {
+            items = win.ajaxResult.tracks;
+        }
+        if (items[e.index].mediaType === 'Audio' || items[e.index].mediaType === 'Video') {
+            ajaxCall('LoginService.ping', null, function(result, error) {
+                if (!result && !error) {
+                    Titanium.UI.createAlertDialog({message:'No response from server, please make sure the server is running.',buttonNames:['Ok']}).show();
+                } else {
+                    if (items[e.index].mediaType === 'Video') {
+                        Titanium.App.fireEvent('mytunesrss_stop');
+                        Titanium.UI.createWindow({url:'win_videoplayer.js',data:items[e.index].playbackUrl,backgroundColor:'#000'}).open();
+                    } else {
+                        Titanium.App.fireEvent('mytunesrss_playlist', {playlist:items,index:e.index});
+                    }
+                }
+            });
         }
     });
-});
+}
+tableView.setData(tableData);
 
 win.add(tableView);
