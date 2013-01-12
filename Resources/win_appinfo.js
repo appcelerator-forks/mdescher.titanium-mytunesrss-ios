a1713323497bf4ace9113969268a8e7887dbc1d9
Michael Descher <mdescher@codewave.de>
2013-01-12 09:41:43 +0000

    appinfo
    git-svn-id: svn://svn.codewave.de/titanium/MyTunesRSS/trunk@7785 6cd82e91-2702-0410-863d-86aa3370aca5

--- Resources/win_appinfo.js
+++ Resources/win_appinfo.js
@@ -14,7 +14,16 @@ function AppInfoWindow() {
 		versionInfo += "\n" + Titanium.Filesystem.getFile('white_noise.wav').modificationTimestamp();
 	}
 
-	win.add(Titanium.UI.createTextArea({editable:false,scrollable:false,top:70,left:20,right:20,color:"#CCCCCC",backgroundColor:DARK_GRAY,font:{fontSize:18},textAlign:"center",value:L("appinfo") + versionInfo}));
+	win.add(GUI.createLabel(STYLE.get("appInfoTitle", {textAlign:Titanium.UI.TEXT_ALIGNMENT_CENTER,text:"MyTunesRSS iOS App"})));
+	win.add(GUI.createLabel(STYLE.get("appInfoVersion", {textAlign:Titanium.UI.TEXT_ALIGNMENT_CENTER,text:"Version " + Titanium.App.version})));
+	var modTs = Titanium.Filesystem.getFile('white_noise.wav').modificationTimestamp();
+	if (Titanium.App.version.indexOf('SNAPSHOT') > 0) {
+		win.add(GUI.createLabel(STYLE.get("appInfoTimestamp", {textAlign:Titanium.UI.TEXT_ALIGNMENT_CENTER,text:modTs})));
+	}
+	win.add(GUI.createLabel(STYLE.get("appInfoCopyright", {textAlign:Titanium.UI.TEXT_ALIGNMENT_CENTER,text:"\u00A9 " + new Date(modTs).getFullYear() + " Codewave Software"})));
+	win.add(GUI.createLabel(STYLE.get("appInfoDesign1", {textAlign:Titanium.UI.TEXT_ALIGNMENT_CENTER,text:"Design: Sebastian Reich"})));
+	win.add(GUI.createLabel(STYLE.get("appInfoDesign2", {textAlign:Titanium.UI.TEXT_ALIGNMENT_CENTER,text:"http://www.graphiq.de"})));
+	win.add(Titanium.UI.createImageView(STYLE.get("appInfoLogo")));
 
 	/**
 	 * Open the app info window. 
