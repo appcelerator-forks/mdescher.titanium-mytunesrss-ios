484c1cec5ad51117103690597ef0bef473e311f4
Michael Descher <mdescher@codewave.de>
2013-06-17 06:55:09 +0000

    use listview instead of tableview
    git-svn-id: svn://svn.codewave.de/titanium/MyTunesRSS/trunk@8029 6cd82e91-2702-0410-863d-86aa3370aca5

--- Resources/win_playlists.js
+++ Resources/win_playlists.js
@@ -5,7 +5,75 @@ function PlaylistsWindow(data) {
 
 	var win = Titanium.UI.createWindow(STYLE.get("window"));
 
-	var tableView = GUI.createTableView(tryGetAdSpacingStyle({search:Titanium.UI.createSearchBar({autocapitalization:false,autocorrect:false,barColor:"#000000"}), filterAttribute:"filter",top:45}));
+	var templateOnline = {
+		childTemplates : [
+			{
+				type : "Titanium.UI.Label",
+				bindId : "main",
+				properties : {
+					left : 10,
+					height : 24,
+					right : 10,
+					font : {
+						fontSize : 20,
+						fontWeight : "bold"
+					},
+					color : "#CCCCCC",
+					minimumFontSize : 12
+				}
+			}
+		]
+	};
+	var templateOffline = {
+		childTemplates : [
+			{
+				type : "Titanium.UI.Label",
+				bindId : "main",
+				properties : {
+					left : 10,
+					height : 24,
+					right : 40,
+					font : {
+						fontSize : 20,
+						fontWeight : "bold"
+					},
+					color : "#CCCCCC",
+					minimumFontSize : 12
+				}
+			},
+			{
+				type : "Titanium.UI.ImageView",
+				bindId : "syncIcon",
+				properties : {
+					width : 20,
+                    right : 10,
+                    image : "images/sync.png",
+                    touchEnabled : false
+				}
+			},
+			{
+				type : "Titanium.UI.View",
+				bindId : "syncGlow",
+				properties : {
+                    GUI.glowViewOptions({right:20})
+				},
+                events : {
+				    "touchstart" : function(e) {
+				    	e.source.setOpacity(0.75);
+				    },
+			  	    "touchend" : function(e) {
+				    	e.source.setOpacity(0);
+				    },
+			  	    "touchcancel" : function(e) {
+				    	e.source.setOpacity(0);
+				    },
+                    "click" : syncClick
+                }
+			}
+		]
+	};
+
+	var listView = GUI.createListView(tryGetAdSpacingStyle({search:Titanium.UI.createSearchBar({autocapitalization:false,autocorrect:false,barColor:"#000000"}),filterAttribute:"filter",top:45,templates:{"online":templateOnline,"offline":templateOffline},defaultItemTemplate:offlineMode ? "offline" : "online"}));
 	var buttonBack = GUI.createButton({title:L("playlists.back"),style:Titanium.UI.iPhone.SystemButtonStyle.BORDERED});
 	
 	buttonBack.addEventListener('click', function() {
@@ -14,85 +82,76 @@ function PlaylistsWindow(data) {
 	});
 	
 	win.add(GUI.createTopToolbar(L("playlists.title"), buttonBack, undefined));	
-	win.add(tableView);
+	win.add(listView);
 	tryAddAd(win);
-	
-	setTableDataAndIndex(
-	        tableView,
+
+    listView.addEventListener("itemclick", function(e) {
+        var busyView = createBusyView();
+        win.add(busyView);
+        Titanium.App.setIdleTimerDisabled(true);
+        try {
+            var itemProps = e.section.getItemAt(e.itemIndex).properties;
+            loadAndDisplayTracks(self, itemProps.tracksUri);
+        } finally {
+            Titanium.App.setIdleTimerDisabled(false);
+            win.remove(busyView);
+        }
+    });
+
+    var syncClick = function(e) {
+	    var busyView = createBusyView();
+	    win.add(busyView);
+	    Titanium.App.setIdleTimerDisabled(true);
+	    try {
+		    var tracks = loadTracks(e.source.tracksUri);
+        	removeObsoleteTracks(tracks);
+	    } finally {
+		    Titanium.App.setIdleTimerDisabled(false);
+	        win.remove(busyView);
+	    }
+        if (tracks != undefined && tracks.length > 0) {
+        	Titanium.Analytics.featureEvent("sync.playlist");
+		    CANCEL_SYNC_AUDIO_TRACKS = false;
+		    var busyWindow = new BusyWindow(L("playlists.busy.syncing"), displayName, function() {
+			    CANCEL_SYNC_AUDIO_TRACKS = true;
+		    });
+		    busyWindow.open();
+		    Titanium.App.setIdleTimerDisabled(true);
+		    var syncProgress = function(e2) {
+			    busyWindow.setProgress(e2.progress);
+		    }
+		    var syncDone = function() {
+			    Titanium.App.setIdleTimerDisabled(false);
+			    busyWindow.close();
+			    Titanium.App.removeEventListener("mytunesrss_sync_progress", syncProgress);
+			    Titanium.App.removeEventListener("mytunesrss_sync_done", syncDone);
+		    }
+		    Titanium.App.addEventListener("mytunesrss_sync_progress", syncProgress);
+		    Titanium.App.addEventListener("mytunesrss_sync_done", syncDone);
+		    Titanium.App.fireEvent("mytunesrss_sync", {data:tracks,index:0});
+        }
+    }
+
+	setListDataAndIndex(
+	        listView,
 	        data,
-	        function(item) {
-	            var displayName = getDisplayName(item.name);
-	            var row = GUI.createTableViewRow({height:TABLE_VIEW_ROW_HEIGHT,className:'playlist_row',selectionStyle:Titanium.UI.iPhone.TableViewCellSelectionStyle.NONE,filter:displayName});
-	            var infoView = Titanium.UI.createView({right:30});
-	            infoView.add(GUI.createLabel({text:displayName,left:10,height:24,right:10,font:{fontSize:20,fontWeight:'bold'}}));
-	            infoView.addEventListener('click', function(e) {
-					var busyView = createBusyView();
-					win.add(busyView);
-					Titanium.App.setIdleTimerDisabled(true);
-                    try {
-				        loadAndDisplayTracks(self, item.tracksUri);
-                    } finally {
-                    	Titanium.App.setIdleTimerDisabled(false);
-				        win.remove(busyView);
+	        function(item, index) {
+	        	return {
+	        		main : {
+	        			text : getDisplayName(item.name)
+	        		},
+                    syncGlow : {
+                        tracksUri : item.tracksUri
                     }
-		    	});
-		        row.add(infoView);
-	            if (!offlineMode) {
-				    var syncImageGlowView = Titanium.UI.createView(GUI.glowViewOptions({right:20}));
-				    var syncImageView = Titanium.UI.createImageView({width:20,image:"images/sync.png",right:10,touchEnabled:false});
-				    var touchView = Titanium.UI.createView({right:0,height:Titanium.UI.FILL,width:40,backgroundColor:"transparent",glow:syncImageGlowView});
-				    row.add(touchView);
-				    touchView.addEventListener("touchstart", function(e) {
-				    	e.source.glow.setOpacity(0.75);
-				    });
-			  	    touchView.addEventListener("touchend", function(e) {
-				    	e.source.glow.setOpacity(0);
-				    });
-			  	    touchView.addEventListener("touchcancel", function(e) {
-				    	e.source.glow.setOpacity(0);
-				    });
-				    touchView.addEventListener("click", function(e) {
-						var busyView = createBusyView();
-						win.add(busyView);
-						Titanium.App.setIdleTimerDisabled(true);
-						try {
-							var tracks = loadTracks(item.tracksUri);
-					    	removeObsoleteTracks(tracks);
-						} finally {
-							Titanium.App.setIdleTimerDisabled(false);
-						    win.remove(busyView);
-						}
-					    if (tracks != undefined && tracks.length > 0) {
-					    	Titanium.Analytics.featureEvent("sync.playlist");
-							CANCEL_SYNC_AUDIO_TRACKS = false;
-				    		var busyWindow = new BusyWindow(L("playlists.busy.syncing"), displayName, function() {
-				    			CANCEL_SYNC_AUDIO_TRACKS = true;
-				    		});
-				    		busyWindow.open();
-				    		Titanium.App.setIdleTimerDisabled(true);
-				    		var syncProgress = function(e) {
-				    			busyWindow.setProgress(e.progress);
-				    		}
-				    		var syncDone = function() {
-				    			Titanium.App.setIdleTimerDisabled(false);
-				    			busyWindow.close();
-				    			Titanium.App.removeEventListener("mytunesrss_sync_progress", syncProgress);
-				    			Titanium.App.removeEventListener("mytunesrss_sync_done", syncDone);
-				    		}
-				    		Titanium.App.addEventListener("mytunesrss_sync_progress", syncProgress);
-				    		Titanium.App.addEventListener("mytunesrss_sync_done", syncDone);
-				    		Titanium.App.fireEvent("mytunesrss_sync", {data:tracks,index:0});
-					    }
-				    });
-				    row.add(syncImageView);
-				    row.add(syncImageGlowView);	            	
-	            }
-	            return row;
+	        		properties : {
+	        			tracksUri : item.tracksUri
+	        		}
+	        	};
 	        },
 	        function(item) {
 	            return item.name;
 	        });
-	
+		
 	/**
 	 * Open the genres window. 
 	 */
