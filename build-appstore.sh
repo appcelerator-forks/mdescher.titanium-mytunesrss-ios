1173aadf79c47b1788d3e165778965657fd030bf
Michael Descher <mdescher@codewave.de>
2015-01-19 11:03:16 +0000

    fixed build scripts
    git-svn-id: svn://svn.codewave.de/titanium/MyTunesRSS/trunk@9130 6cd82e91-2702-0410-863d-86aa3370aca5

--- build-appstore.sh
+++ build-appstore.sh
@@ -1,2 +1,2 @@
-titanium build -b -p ios -F universal -T dist-appstore -P F69808BE-62CA-4F47-B1AC-13515B3E8CA6 -R "Michael Descher"
+titanium build -b -p ios -F universal -T dist-appstore -P a2e62e86-8086-40c0-8cdb-63af6efa048b -R "Michael Descher (64Q697M329)"
 
--- build-device.sh
+++ build-device.sh
@@ -1,2 +1,2 @@
-titanium build -b -p ios -F universal -T device -P DE4B0D1A-C2AC-4548-8469-072438617BD0 -V "Michael Descher"
+titanium build -b -p ios -F universal -T device -P 73f552b3-5bdd-4adc-a63f-6450fb6fc5b9 -V "Michael Descher (KA3C22NAMD)"
 
